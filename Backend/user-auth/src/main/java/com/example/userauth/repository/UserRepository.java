2f2dfae9e7f31b234b62734ccb4b51ad804fcc24
916-Sandor-Alexandra <72196242+916-Sandor-Alexandra@users.noreply.github.com>
2022-06-11 15:58:59 +0300

    Added backend

---
+++ Backend/user-auth/src/main/java/com/example/userauth/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.userauth.repository;
+
+import com.example.userauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
